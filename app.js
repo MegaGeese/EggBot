3e7459e92a8eaffc3cca63b7b64aa85ad8c54476
Glitch (splendid-noiseless-sled) <none>
2023-11-07 20:33:49 +0000

    🌅🎱 Checkpoint ./app.js:62756368/9289 ./game:62756368/2634 ./game.js:62756368/5268 ./commands.js:62756368/512

--- app.js
+++ app.js
@@ -41,24 +41,36 @@ app.post('/interactions', async function (req, res) {
     const { name } = data;
     // "test" command
     if (name === 'schedulematch') {
-      console.log(res)
       // Send a message into the channel where command was triggered from
+      const userId = req.body.member.user.id;
+      // User's object choice
+      const objectName = req.body.data.options[0].value;
+
+      // Create active game using message ID as the game ID
+      activeGames[id] = {
+          id: userId,
+          objectName,
+      };
+      
       return res.send({
-        "title": "My Cool Modal",
-        "custom_id": "cool_modal",
-        "components": [{
-          "type": 1,
-          "components": [{
-            "type": 4,
-            "custom_id": "name",
-            "label": "Name",
-            "style": 1,
-            "min_length": 1,
-            "max_length": 4000,
-            "placeholder": "John",
-            "required": true
-          }]
-        }]
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+            content: `Rock papers scissors challenge from <@${userId}>`,
+            components: [
+            {
+                type: MessageComponentTypes.ACTION_ROW,
+                components: [
+                {
+                    type: MessageComponentTypes.BUTTON,
+                    // Append the game ID to use later on
+                    custom_id: `accept_button_${req.body.id}`,
+                    label: 'Accept',
+                    style: ButtonStyleTypes.PRIMARY,
+                },
+                ],
+            },
+            ],
+        },
       });
     }
   }
--- commands.js
+++ commands.js
@@ -1,11 +1,36 @@
 import 'dotenv/config';
+import { getRPSChoices } from './game.js';
 import { capitalize, InstallGlobalCommands } from './utils.js';
+
+// Get the game choices from game.js
+function createCommandChoices() {
+  const choices = getRPSChoices();
+  const commandChoices = [];
+
+  for (let choice of choices) {
+    commandChoices.push({
+      name: capitalize(choice),
+      value: choice.toLowerCase(),
+    });
+  }
+
+  return commandChoices;
+}
 
 // Simple test command
 const TEST_COMMAND = {
   name: 'schedulematch',
   description: 'Add match to the #match-schedule channel',
   type: 1,
+  options:[
+    {
+      type: 3,
+      name: 'object',
+      description: 'Pick your object',
+      required: true,
+      choices: createCommandChoices(),
+    },
+  ],
 };
 
 const ALL_COMMANDS = [TEST_COMMAND];
